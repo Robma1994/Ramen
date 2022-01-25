28e9483329942fcffab5cdf4b0956bb1cce4426e
Roberto <robertomartinonwg@gmail.com>
2022-01-25 12:54:28 +0100

    Struttura 1 con logica slider click

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const app = new Vue({
     el: '#root',
     data: {
-        
+        images: ['ciao','ciop','flip','ciao','ciop','flip','ciao','ciop','flip',],
+        count: 0,
+        off: 'none'
     },
-
+    methods: {
+        chooise(index){
+            this.count = index
+            this.off = 'block'
+        }
+    }
 })
+

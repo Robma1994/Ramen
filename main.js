fba589f2c6a12be28669750ad87d48e00b6e485d
Roberto <robertomartinonwg@gmail.com>
2022-01-26 09:20:36 +0100

    Seconda bozza completata

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const app = new Vue({
     el: '#root',
     data: {
-        images: ['ciao','ciop','flip','ciao','ciop','flip','ciao','ciop','flip',],
+        images: [
+            {
+                link: '_mountains',
+                name: 'Neve',
+                icon: 'fas fa-snowflake'
+            },
+            {
+                link: '_rain',
+                name: 'Pioggia',
+                icon: 'fas fa-cloud-rain'
+            },
+            {
+                link: '_sun',
+                name: 'Sole',
+                icon: 'fas fa-sun'
+            },
+        ],
         count: 0,
-        off: 'none'
+        off: 'none',
+        on:'block',
     },
     methods: {
         chooise(index){
